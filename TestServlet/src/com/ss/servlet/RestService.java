25e80373a550c89d3b1d28794aed8dbb63ce30af
Matt <mgmcintyre777@gmail.com>
2020-04-30 08:32:22 -0400

    should work

--- TestServlet/src/com/ss/servlet/RestService.java
+++ TestServlet/src/com/ss/servlet/RestService.java
@@ -1,13 +1,6 @@
 package com.ss.servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -15,9 +8,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.ss.dto.User;
-import com.google.gson.Gson;
 
-@WebServlet({ "/user", "/user/", "/user/id/*" })
+@WebServlet({ "/login" })
 public class RestService extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
